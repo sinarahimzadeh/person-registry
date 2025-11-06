ef388e154c1ff4ac64812993f827dd6e90e27302
sinajoonunipd <sina.rahimzadeh@studenti.unipd.it>
2025-11-06 16:05:53 +0100

    setting up springboot

---
+++ backend/backend/src/main/java/com/personregistry/backend/PersonRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.personregistry.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistryApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/personregistry/backend/PersonRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personregistry.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
