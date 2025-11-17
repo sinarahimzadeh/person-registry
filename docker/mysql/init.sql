bc07aa3b4443441b7028d25857b4631b7bf18aa1
sinajoonunipd <sina.rahimzadeh@studenti.unipd.it>
2025-11-17 14:29:52 +0100

    Fixed FK

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,19 +1,21 @@
 CREATE DATABASE IF NOT EXISTS person_registry;
 USE person_registry;
 
-CREATE TABLE pr_person (
-  tax_code VARCHAR(16) PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  surname VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE pr_address (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  person_tax_code VARCHAR(16) NOT NULL UNIQUE,
-  street VARCHAR(200) NOT NULL,
-  street_no VARCHAR(20) NOT NULL,
-  city VARCHAR(100) NOT NULL,
-  province CHAR(2) NOT NULL,
-  country VARCHAR(100) NOT NULL,
-  FOREIGN KEY (person_tax_code) REFERENCES pr_person(tax_code) ON DELETE CASCADE
+  street      VARCHAR(200) NOT NULL,
+  street_no   VARCHAR(20)  NOT NULL,
+  city        VARCHAR(100) NOT NULL,
+  province    CHAR(2)      NOT NULL,
+  country     VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pr_person (
+  tax_code VARCHAR(16) PRIMARY KEY,
+  name     VARCHAR(100) NOT NULL,
+  surname  VARCHAR(100) NOT NULL,
+  address_id BIGINT,
+  CONSTRAINT fk_person_address
+    FOREIGN KEY (address_id)
+    REFERENCES pr_address(id)
 );
